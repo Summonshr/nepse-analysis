b006abd16819714282663bd11298edb028c83288
suman shrestha <summonshr@gmail.com>
2018-07-09 13:16:40 +0545

    working

--- resources/views/articles/what-is-stock.blade.php
+++
@@ -1,4 +0,0 @@
-<p>
-    A stock is a general term used to describe the ownership certificates of any company. A share, on the other hand, refers to the stock certificate of a particular company. Holding a particular company's share makes you a shareholder.
-</p>
-<p>Stocks are of two types—common and preferred. The difference is while the holder of the former has voting rights that can be exercised in corporate decisions, the later doesn't. However, preferred shareholders are legally entitled to receive a certain level of dividend payments before any dividends can be issued to other shareholders.</p>
