de7ea36444ec7a1e19ad7277756cfff6157fd61b
Suman Shrestha <summonshr@gmail.com>
2019-03-16 20:55:00 +0545

    WIP

--- app/Http/Controllers/History.php
+++ app/Http/Controllers/History.php
@@ -8,6 +8,12 @@ class History extends Controller
 {
     public function __invoke($companyCode = null)
     {
-        return collect()->put('histories', \App\Company::where('code', $companyCode)->first()->history()->select(['amount', 'closing_price', 'date', 'no_of_transaction', 'traded_shares', 'max_price', 'min_price'])->get());
+        $json = cache()->remember('history-'.$companyCode, 36000, function() use ($companyCode) {
+            return collect()->put('histories', \App\Company::where('code', $companyCode)->first()->history()->select(['amount', 'closing_price', 'date', 'no_of_transaction', 'traded_shares', 'max_price', 'min_price'])->get());
+        });
+        return response()
+            ->json($json)
+            ->header("pragma", "private")
+            ->header("Cache-Control", " private, max-age=86400");
     }
 }
