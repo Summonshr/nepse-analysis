1ebb268d53b78372cb06638709486b400eac18fe
Suman Shrestha <summonshr@gmail.com>
2019-03-17 20:20:50 +0545

    WIP

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::group(['middleware'=>'cache'], function(){
 Route::get('report','ReportController@index');
 Route::get('report/{code}','ReportController@show');
 Route::put('report/{code}','ReportController@update');
+Route::get('sample',function(){
+    Artisan::call('scrape:todays-share-price');
+    Artisan::call('scrape:company-list');
+    Artisan::call('cache:clear');
+    return redirect("report.json");
+});
