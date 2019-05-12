3fbc748b131ee34bbfce57fce8e60d6f42f4f9b8
Suman Shrestha <summonshr@gmail.com>
2019-05-12 15:43:31 +0545

    WIP

--- app/Http/Controllers/Movements.php
+++ app/Http/Controllers/Movements.php
@@ -8,29 +8,31 @@ class Movements extends Controller
 {
     public function display()
     {
-        $crawler = Goutte::request('POST', 'http://www.nepalstock.com/floorsheet', ['_limit' => 100000]);
-        $floorSheet = collect([]);
-        $crawler->filter('tr')->each(function ($node, $index) use (&$floorSheet) {
+        return cache()->remember('floor-sheet-final', 1 * 60 * 60 * 4, function () {
+            return $crawler = Goutte::request('POST', 'http://www.nepalstock.com/floorsheet', ['_limit' => 100000]);
+            $floorSheet = collect([]);
+            $crawler->filter('tr')->each(function ($node, $index) use (&$floorSheet) {
 
-            if ($index < 2) {
-                return;
-            }
+                if ($index < 2) {
+                    return;
+                }
 
-            $arr = [];
+                $arr = [];
 
-            $node->filter('td')->each(function ($node, $index) use (&$arr) {
-                array_push($arr, $node->html());
+                $node->filter('td')->each(function ($node, $index) use (&$arr) {
+                    array_push($arr, $node->html());
+                });
+
+                $arrKeys = ['sn', 'number', 'code', 'buyer', 'seller', 'quantity', 'rate', 'total'];
+
+                count($arr) == count($arrKeys) && $floorSheet->push(collect($arrKeys)->combine($arr));
             });
 
-            $arrKeys = ['sn', 'number', 'code', 'buyer', 'seller', 'quantity', 'rate', 'total'];
-
-            count($arr) == count($arrKeys) && $floorSheet->push(collect($arrKeys)->combine($arr));
-        });
-
-        return $floorSheet->filter(function ($sheet) {
-            return $sheet->get('rate') > 220 && $sheet->get('total') > 15000;
-        })->sortByDesc(function ($row) {
-            return $row->get('total');
-        })->take(50)->values();
+            return $floorSheet->filter(function ($sheet) {
+                return $sheet->get('rate') > 220 && $sheet->get('total') > 15000;
+            })->sortByDesc(function ($row) {
+                return $row->get('total');
+            })->take(50)->values();;
+        })
     }
 }
