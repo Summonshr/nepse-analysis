cc482818aab2021c058590ca46c4f543ae9d0436
Suman Shrestha <summonshr@gmail.com>
2019-05-25 11:30:38 +0545

    WIP

--- app/Http/Controllers/Company.php
+++ app/Http/Controllers/Company.php
@@ -8,7 +8,7 @@ class Company extends Controller
 {
     public function __invoke($companyCode = null)
     {
-        return collect()->put('companies', $companyCode ? array_only(\App\Company::with('report')->here('code', $companyCode)->first()->toArray(), ['name','report','profile','type']) : cache()->remember('companies-all-' . request('minimal', 5), 10, function () {
+        return collect()->put('companies', $companyCode ? array_only(\App\Company::with('report')->where('code', $companyCode)->first()->toArray(), ['name','report','profile','type']) : cache()->remember('companies-all-' . request('minimal', 5), 10, function () {
             return \App\Company::with('dividends')->get()->map->toApi();
         }));
     }
